5dc8515f2970bf0843de0febb08ebec91f395150
Riccardo Tommasini <riccardo.tommasini@polimi.it>
2018-05-23 09:01:40 +0200

    future work on using annotations instead of interfaces

---
+++ src/main/java/it/polimi/rsp/vocals/annotations/model/Deletable.java
@@ -0,0 +1,19 @@
+package it.polimi.rsp.vocals.annotations.model;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * This annotation should be put on serivces models to ensure
+ * the creation of an endpoint that allows their retrieval
+ *
+ * **/
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Deletable {
+
+    String name();
+
+}
