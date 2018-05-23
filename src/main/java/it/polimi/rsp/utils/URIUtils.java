b2fa23ec69e45328c720206f32b694eaeefc64e9
Riccardo Tommasini <riccardo.tommasini@polimi.it>
2018-05-23 09:03:26 +0200

    utility for uri management

--- src/main/java/it/polimi/rsp/utils/URIUtils.java
+++ src/main/java/it/polimi/rsp/utils/URIUtils.java
@@ -1,9 +1,22 @@
 package it.polimi.rsp.utils;
 
+import it.polimi.rsp.vocals.annotations.features.RSPService;
+
+import java.lang.reflect.Method;
+
 public class URIUtils {
 
     /* Properties */
     public static final String SLASH = "/";
 
+    public static final String COLON = ":";
 
+    public static String addParam(String uri, String param) {
+        return uri + SLASH + COLON + param;
+    }
+
+    public static String build(Method m, String base, String param) {
+        RSPService service = m.getAnnotation(RSPService.class);
+        return base + SLASH + service + SLASH + param;
+    }
 }
